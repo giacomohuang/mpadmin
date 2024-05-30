c6a286842a35b43b33368ef4bffb759920c7977f
giacomohuang <giacomohuang@qq.com>
2024-05-30 19:37:31 +0800

    update

--- src/views/Workspace/Main.vue
+++ src/views/Workspace/Main.vue
@@ -1,5 +1,15 @@
-<template>hello world!</template>
+<template>
+   <a-checkbox-group v-model:value="pList" style="width: 100%" :options="permissionData" @change="getValue">
+  </a-checkbox-group>
+</template>
 
-<script setup></script>
+<script setup>
+import {ref} from 'vue'
+import {permissionData} from './permissiondata'
+const pList=ref([]) 
+function getValue(c){
+  console.log(c,pList)
+}
+</script>
 
 <style lang="scss" scoped></style>

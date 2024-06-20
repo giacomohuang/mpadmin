637fe59373911c78f51d3f3969c2383b50683b2f
giacomohuang <giacomohuang@qq.com>
2024-06-20 19:22:18 +0800

    update

--- src/views/My/Authentication.vue
+++ src/views/My/Authentication.vue
@@ -2,19 +2,20 @@
   <div class="main">
     <section>
       <div class="title flex">
-        <h2>密码</h2>
+        <h2>{{ $t('authentication.pwd') }}</h2>
+        Strengthen your account by ensuring your password is strong.
         <a-button @click.stop="status.toggleChangePwd = !status.toggleChangePwd">{{ status.toggleChangePwd ? $t('authentication.hide') : $t('authentication.changepwd') }}</a-button>
       </div>
       <div v-if="status.toggleChangePwd" class="content">
-        <a-form :model="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 12 }">
-          <a-form-item label="旧密码">
-            <a-input type="password" v-model:value="form.oldPassword" />
+        <a-form ref="pwdFormRef" :model="pwdForm" :label-col="{ span: 6 }" :rules="pwdRules" :wrapper-col="{ span: 12 }" @validate="handlePwdValidate">
+          <a-form-item has-feedback :label="$t('authentication.oldpwd')" name="oldPassword">
+            <a-input type="password" v-model:value="pwdForm.oldPassword" />
           </a-form-item>
-          <a-form-item label="新密码">
-            <a-input type="password" v-model:value="form.newPassword" />
+          <a-form-item has-feedback :label="$t('authentication.newpwd')" name="newPassword">
+            <a-input type="password" v-model:value="pwdForm.newPassword" />
           </a-form-item>
-          <a-form-item label="确认新密码">
-            <a-input type="password" v-model:value="form.comfirmPassword" />
+          <a-form-item has-feedback :label="$t('authentication.confirmpwd')" name="confirmPassword">
+            <a-input type="password" v-model:value="pwdForm.confirmPassword" />
           </a-form-item>
           <a-form-item :wrapper-col="{ offset: 6 }">
             <a-button type="primary">{{ $t('common.submit') }}</a-button>
@@ -26,10 +27,50 @@
 </template>
 <script setup>
 import { reactive, ref } from 'vue'
-const form = ref({})
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+const pwdFormRef = ref()
+const pwdForm = reactive({
+  oldPassword: '',
+  newPassword: '',
+  confirmPassword: ''
+})
+
 const status = reactive({
   toggleChangePwd: false
 })
+
+const vPwd = async (_rule, value) => {
+  console.log('aaaa')
+  if (value === '') {
+    return Promise.reject(t('authentication.pep'))
+  } else {
+    if (pwdForm.confirmPassword !== '') {
+      pwdFormRef.value.validateFields('confirmPassword')
+    }
+    return Promise.resolve()
+  }
+}
+const vConfirmPwd = async (_rule, value) => {
+  console.log('bbbb')
+  if (value === '') {
+    return Promise.reject(t('authentication.pepa'))
+  } else if (value !== pwdForm.newPassword) {
+    return Promise.reject(t('authentication.pnm'))
+  } else {
+    return Promise.resolve()
+  }
+}
+
+const pwdRules = {
+  newPassword: [{ required: true, validator: vPwd, trigger: 'change' }],
+  confirmPassword: [{ validator: vConfirmPwd, trigger: 'change' }]
+}
+
+const handlePwdValidate = (...args) => {
+  console.log('handle')
+  console.log(args)
+}
 </script>
 <style lang="scss" scoped>
 @import '../../assets/page.scss';

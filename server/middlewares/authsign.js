84ccbcfaa177465751d389c16b8440625c94c5fe
giacomohuang <giacomohuang@qq.com>
2024-07-04 16:36:55 +0800

    remove server

--- server/middlewares/authsign.js
+++
@@ -1,9 +0,0 @@
-const Redis = require('ioredis')
-const crypto = require('crypto')
-
-const authSign = async (ctx, next) => {
-  // console.log('authSign')
-  await next()
-}
-
-module.exports = authSign
